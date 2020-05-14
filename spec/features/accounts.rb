a573aff1076cb7909642d649d58c89d8779ea4d5
summas <masa.sumi.w@gmail.com>
2020-05-14 16:23:02 +0900

    add FactoryBot

---
+++ spec/features/accounts.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :account do
+    id {1}
+    email {"qwerty@gmail.com"}
+    password {"qwerty"}
+ #   created_at: nil
+ #   updated_at: nil
+ #   name: nil
+ #   auth: nil
+ #   organization: nil
+  end
+end
