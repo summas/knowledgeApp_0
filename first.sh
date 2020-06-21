b08be39a371109a471581e27c345dfe8a77ffca2
masasm <masa.sumi.w@gmail.com>
2020-06-21 10:50:41 +0000

    modified production mode function.

---
+++ first.sh
@@ -0,0 +1,14 @@
+bundle install
+apt-get update && apt-get install -y curl apt-transport-https wget && \
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+apt-get update && apt-get install -y yarn
+
+yarn install --check-files
+
+
+rails webpacker:install
+
+rails action_text:install
+
+rails assets:precompile
