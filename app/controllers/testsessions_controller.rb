f8bdc2d28568047001033936dd09e292b0e9191b
masasm <masa.sumi.w@gmail.com>
2020-06-23 11:56:33 +0000

    modified some views.

--- app/controllers/testsessions_controller.rb
+++ app/controllers/testsessions_controller.rb
@@ -1,8 +1,6 @@
 class TestsessionsController < ApplicationController
   def create
     account = Account.find_by(email: 'test@example.com')
-    puts 'テストセッション'
-    puts account.id
     session[:account_id] = account.id
     flash[:success] = 'テストユーザとしてログインしました。'
     sign_in account
