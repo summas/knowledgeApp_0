87b37bb1f385b8cbe0f176748cdabb7a63a68f2e
masasm <masa.sumi.w@gmail.com>
2020-07-09 10:36:53 +0000

    Add localhosts.

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module KnowledgeApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.hosts << 'knwldg.xyz'
+    config.hosts << 'localhost'
   end
 end
