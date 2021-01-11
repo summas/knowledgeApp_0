7bb9b25df665907c723b2bfd2e34b115a7dce895
summas <masa.sumi.w@gmail.com>
2021-01-11 20:54:51 +0900

    test:Add controller spec

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+    describe 'GET /index' do
+        it 'returns http success' do
+          get :index
+          expect(response).to have_http_status '200'
+        end
+    end
+
+    describe 'GET /show' do
+      it 'returns http success' do
+        FactoryBot.create(:category)
+
+        FactoryBot.create(:disclosureRange)
+    
+        FactoryBot.create(:group)
+        FactoryBot.create(:article)
+        get :show, params: { id: 1000 }
+        expect(response).to have_http_status '200'
+      end
+    end
+
+end
