364db1634838ca688dcbc6dea0c42f5f45563f2b
summas <masa.sumi.w@gmail.com>
2021-01-11 21:00:48 +0900

    fix:Adapted to rubocop

--- spec/controllers/testsessions_controller_spec.rb
+++ spec/controllers/testsessions_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TestsessionsController, type: :controller do
-    describe 'GET /create' do
-        # FactoryBot.create(:account)
-        it 'returns http success' do
-          post :create
-          expect(response).to redirect_to '/'
-        end
-      end
+  describe 'GET /create' do
+    # FactoryBot.create(:account)
+    it 'returns http success' do
+      post :create
+      expect(response).to redirect_to '/'
+    end
+  end
 end
