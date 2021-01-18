e1d2b3f213dd64113f6a980da333017a48ddacdf
summas <masa.sumi.w@gmail.com>
2021-01-18 20:41:07 +0900

    test:fix for rubocop

--- spec/controllers/articles_edits_controller_spec.rb
+++ spec/controllers/articles_edits_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ArticleEditsController, type: :controller do
+  before do
+    @account = FactoryBot.create(:account)
+    FactoryBot.create(:category)
+    FactoryBot.create(:disclosureRange)
+    FactoryBot.create(:group)
+  end
+
   describe 'GET /index' do
     it 'returns http status 302' do
       get :index
@@ -8,8 +15,7 @@ RSpec.describe ArticleEditsController, type: :controller do
     end
 
     it 'returns http success as an authorized user' do
-      @user = FactoryBot.create(:account)
-      sign_in @user
+      sign_in @account
       get :index
       expect(response).to have_http_status '200'
     end
@@ -17,15 +23,13 @@ RSpec.describe ArticleEditsController, type: :controller do
 
   describe 'GET /new' do
     it 'returns http success as an authorized user' do
-      @user = FactoryBot.create(:account)
-      sign_in @user
+      sign_in @account
       get :new
       expect(response).to have_http_status '200'
     end
 
     it 'post' do
-      @user = FactoryBot.create(:account)
-      sign_in @user
+      sign_in @account
       get :new
       expect(response).to have_http_status '200'
     end
@@ -33,53 +37,41 @@ RSpec.describe ArticleEditsController, type: :controller do
 
   describe 'post /create' do
     it 'post' do
-      @account = FactoryBot.create(:account)
       articles_params = FactoryBot.attributes_for(:article)
       sign_in @account
-      # post :create, params: { title: @article.title, content: @article.content,  category_id: @article.id ,group_id: @article.group_id ,account_id:@article.account_id ,disclosureRange_id:@article.disclosureRange_id, image: @article.image}
-      post :create, params: { article:articles_params }
+      # post :create, params: { title: @article.title, content: @article.content,  category_id: @article.id
+      # ,group_id: @article.group_id ,account_id:@article.account_id ,disclosureRange_id:@article.disclosureRange_id, image: @article.image}
+      post :create, params: { article: articles_params }
       expect(response).to redirect_to root_path
     end
   end
 
   describe 'post /edit' do
     it 'patch' do
-      @account = FactoryBot.create(:account)
-      FactoryBot.create(:category)
-      FactoryBot.create(:disclosureRange)
-      FactoryBot.create(:group)
       @article = FactoryBot.create(:article)
       articles_params = FactoryBot.attributes_for(:article)
       sign_in @account
-      patch :edit, params: { article:articles_params, id: @article.id }
+      patch :edit, params: { article: articles_params, id: @article.id }
       expect(response).to redirect_to root_path
     end
   end
 
   describe 'patch /edit' do
     it 'patch' do
-      @account = FactoryBot.create(:account)
-      FactoryBot.create(:category)
-      FactoryBot.create(:disclosureRange)
-      FactoryBot.create(:group)
       @article = FactoryBot.create(:article)
       articles_params = FactoryBot.attributes_for(:article)
       sign_in @account
-      patch :edit, params: { article:articles_params, id: @article.id }
+      patch :edit, params: { article: articles_params, id: @article.id }
       expect(response).to redirect_to root_path
     end
   end
 
   describe 'post / destroy' do
     it 'patch' do
-      @account = FactoryBot.create(:account)
-      FactoryBot.create(:category)
-      FactoryBot.create(:disclosureRange)
-      FactoryBot.create(:group)
       @article = FactoryBot.create(:article)
       articles_params = FactoryBot.attributes_for(:article)
       sign_in @account
-      patch :edit, params: { article:articles_params, id: @article.id }
+      patch :edit, params: { article: articles_params, id: @article.id }
       expect(response).to redirect_to root_path
     end
   end
